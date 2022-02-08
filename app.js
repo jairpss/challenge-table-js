fc9bd010ad8090d268c1e57907efb4a91c2d509e
Jair Partida <jairpartida1@gmail.com>
2022-02-07 21:07:50 -0700

    challenge done

--- app.js
+++ app.js
@@ -1,39 +1,51 @@
+let table = document.getElementById('table')
+let rowElement = document.getElementById('input-row')
+let columnElement = document.getElementById('input-column')
+
+function isValid() {
+    let row = rowElement.value
+    let column = columnElement.value
+
+    if(row.length || column.length == 0){
+        alert('Number of rows/columns cannot be empty')
+        return false
+    }
+
+    if( parseInt(row) || parseInt(column) > 9) {
+        alert('Number of rows/columns cannot be greater than 9')
+        return false
+    }
+
+    if(parseInt(row) || parseInt(column) <= 0) {
+        alert('Number of rows/columns cannot be less than 1')
+        return false
+    }
+    return true
+}
+
 function createTable(){
-    let table = document.getElementById('table')
-    let row = document.getElementById('input-row').value
-    let column = document.getElementById('input-column').value
+    let row = rowElement.value
+    let column = columnElement.value
 
-     for(let rowIndex = 0; rowIndex < row; rowIndex++){
-        let tr = document.createElement('tr')
+    if(isValid(row))
+    {
+      for(let rowIndex = 0; rowIndex < row; rowIndex++)
+      {
+          let tr = document.createElement('tr')
 
-        for(let colIndex = 0; colIndex < column; colIndex++){
-            let td = document.createElement('td')
-            tr.appendChild(td)
-        }
+          for(let colIndex = 0; colIndex < column; colIndex++){
+              let td = document.createElement('td')
+              tr.appendChild(td)
+          }
 
-        table.appendChild(tr)
-
-        document.getElementById('input-row').value = " "
-        document.getElementById('input-column').value = " "
-
-    //  if(row > 9) {
-    //      alert('Number of rows cannot be higher than 9')
-    //     //  document.getElementById('input-row').value = " "
-    //     //  console.log("warning")
-    //  }
-    //  if(column > 9) {
-    //      alert('Number of columns cannot be higher than 9')
-    //      document.getElementById('input-column').value = " "
-    //      console.log("warning")
-    //  }
+          table.appendChild(tr)
+          document.getElementById('input-row').value = ""
+          document.getElementById('input-column').value = ""
+      }
     }
 }
-// if(row > 9) {
-//     alert('Number of rows cannot be higher than 9')
-// }
-// if(column > 9) {
-//     alert('Number of columns cannot be higher than 9')
-// }
-    
 
-
+//Provisional function
+function resetTable(){
+    window.location.reload()
+}
